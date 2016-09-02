35ce4f7ee53a4a9c483f7a85b223692015cf1503
sara-02 <sarahmasud02@gmail.com>
2016-09-02 22:53:10 +0530

    Adding comments for julia.

---
+++ Comments_in_julia.jl
@@ -0,0 +1,11 @@
+# Hello World, I am julia!
+
+#= Multiline comments in julia
+	are written in betwenn "#=<comments...>=#"
+	
+=#
+#= After installing julia run this as follows:
+	pathto/julia julia pathto/Comments_in_julia
+=#
+
+println("You should comment your code.")		
